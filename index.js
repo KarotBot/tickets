d20cadea945e9b0f54aa367ef9e8b816d1fbcdd5
Slenky <slenky@protonmail.com>
2021-06-05 15:16:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ client.tickets = {
 }
 
 client.on('ready', async () => {
+    client.user.setActivity({ type: "COMPETING", name: `https://docs.karot.xyz`});
     const GCommandsClient = new GCommands(client, {
         cmdDir: "commands/",
         eventDir: "events/",
